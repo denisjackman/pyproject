2dfe123abd97f84e1eeb9cbe3aeebcea136944da
Denis Jackman <denis.jackman@gmail.com>
2022-06-04 10:10:35 +0100

    fixing pylint for the latest example

--- spotify.py
+++ spotify.py
@@ -5,18 +5,25 @@
 import tekore as tk
 from jackmanimation import credscheck
 
-cred_id = credscheck()
-client_id = cred_id["SpotifyClientID"]
-client_secret = cred_id["SpotifyClientSecret"]
-redirect_uri = "https://github.com/denisjackman/pyproject"
-# conf = (client_id, client_secret, redirect_uri)
-token = cred_id["SpotifyToken"]
-spotify = tk.Spotify(token)
-tracks = spotify.current_user_top_tracks()
-count = 1
-for track in tracks.items:
-    print(count, track.name)
-    count = count + 1
-user = spotify.user
-print(user.display_name())
-# spotify.playback_start_tracks([t.id for t in tracks.items])
+
+def main():
+    """ This is the main function """
+    cred_id = credscheck()
+    # client_id = cred_id["SpotifyClientID"]
+    # client_secret = cred_id["SpotifyClientSecret"]
+    # redirect_uri = "https://github.com/denisjackman/pyproject"
+    # conf = (client_id, client_secret, redirect_uri)
+    token = cred_id["SpotifyToken"]
+    spotify = tk.Spotify(token)
+    tracks = spotify.current_user_top_tracks()
+    count = 1
+    for track in tracks.items:
+        print(count, track.name)
+        count = count + 1
+        user = spotify.user
+        print(user.display_name())
+    # spotify.playback_start_tracks([t.id for t in tracks.items])
+
+
+if __name__ == '__main__':
+    main()
