dffca7d3fb4de267b5e7e70354a2099dc14f23e5
Denis Jackman <denis_jackman@hotmail.com>
2022-06-07 13:18:28 +0100

    last one I promise

--- slack/slack_bot.py
+++ slack/slack_bot.py
@@ -4,6 +4,7 @@
 """
 import os
 import sys
+import argparse
 
 #pylint: disable=wrong-import-position
 MODULE_PATH = "../module/"
@@ -11,7 +12,6 @@ sys.path.append(os.path.abspath(MODULE_PATH))
 from jackmanimation import credscheck
 #pylint: enable=wrong-import-position
 
-import argparse
 import requests
 
 
