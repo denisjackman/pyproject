18cdfa4ebe4993860e8ecafd27dc50d926cb0986
Denis Jackman <denis_jackman@hotmail.com>
2022-07-04 23:50:32 +0100

    Added some data science stuff

---
+++ code/WhatsApp/Whatsapp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+'''
+whatsapp.py
+
+'''
+
+__author__ = "Denis J Jackman (denis_jackman@hotmail.com)"
+__version__ = "$Revision: 0.00 $"
+__date__ = "$Date: 2022/07/04 12:03:00 $"
+__copyright__ = "Copyright (c) 2022 Denis J Jackman"
+__license__ = "Python"
+
+import pywhatkit as kit
+kit.sendwhatmsg("+447841982168", "test message from my desktop", 13, 18, True, 5)
