02b74b24811854b8f39ec650d54dbc3e48d14ae4
Denis Jackman <denis_jackman@hotmail.com>
2022-07-13 13:17:04 +0100

    daily update

--- code/game/newgame/newgame.py
+++ code/game/newgame/newgame.py
@@ -9,11 +9,12 @@ from pygame.locals import (QUIT, KEYDOWN, K_ESCAPE)  # pylint: disable=C0413
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-WIDTH = 410
-HEIGHT = 304
+WIDTH = 800
+HEIGHT = 600
 CAPTION = 'New Game'
 SCREEN_SIZE = (WIDTH, HEIGHT)
-IMG_FILE = 'y:/Resources/images/match3_sheet_0.png'
+IMG_FILE = 'y:/Resources/images/match3_db16_2.png'
+IMG_FILE_2 = 'y:/pyproject/resources/match3_tiles_px.png'
 ICON_FILE = 'y:/pyproject/resources/icon.png'
 SPRITE_X = 0
 SPRITE_Y = 0
@@ -22,7 +23,8 @@ pygame.init()
 
 game_screen = pygame.display.set_mode(SCREEN_SIZE)
 icon = pygame.image.load(ICON_FILE)
-imgload = pygame.image.load(IMG_FILE).convert()
+imgload = pygame.image.load(IMG_FILE_2).convert()
+second_set = pygame.image.load(IMG_FILE).convert()
 
 pygame.display.set_caption(CAPTION)
 pygame.display.set_icon(icon)
@@ -30,11 +32,11 @@ pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 
 
-def sprite(sprite_surface, sx, sy):
+def sprite(sprite_surface, sprite_position):
     '''
         sprite function
     '''
-    game_screen.blit(sprite_surface, (sx, sy))
+    game_screen.blit(sprite_surface, sprite_position)
 
 
 def sprite_load(source_surface,
@@ -73,17 +75,69 @@ def main():
         main routine
     '''
     imgload.set_colorkey(BLACK)
-    sprite_img = sprite_load(imgload, (5, 5, 35, 35), (30, 30))
-    sprite_img2 = sprite_load(imgload, (5, 170, 35, 205), (30, 30))
-    sprite_img3 = sprite_load(imgload, (37, 5, 67, 35), (30, 30))
+
+    start_posx = 0
+    start_posy = 0
+    sprite_width = 40
+    sprite_height = 40
+    sprite_size = (sprite_width, sprite_height)
+
+    sprite_list = []
+    cur_posx = start_posx
+    cur_posy = start_posy
+
+    for _ in range(2):
+        for _ in range(8):
+            sprite_pos = (cur_posx,
+                          cur_posy,
+                          sprite_width + cur_posx,
+                          sprite_height + cur_posy )
+            sprite_list.append(sprite_load(imgload, sprite_pos, sprite_size))
+            cur_posx += sprite_width
+        cur_posy += sprite_height
+        cur_posx = 0
+    cur_posx = start_posx
+    cur_posy = start_posy
+    nu_sprite_width = 16
+    nu_sprite_height = 16
+    nu_sprite_size = (nu_sprite_width, nu_sprite_height)
+    for _ in range(16):
+        for _ in range(6):
+            sprite_pos = (cur_posx,
+                          cur_posy,
+                          nu_sprite_width + cur_posx,
+                          nu_sprite_height + cur_posy )
+            sprite_img2 = sprite_load(second_set, sprite_pos, nu_sprite_size)
+            sprite_list.append(pygame.transform.scale(sprite_img2, (40, 40)))
+            cur_posx += nu_sprite_width
+        cur_posy += nu_sprite_height
+        cur_posx = 0
+
 
     RUNNING = True
+
     while RUNNING:
         RUNNING = check_status()
-        game_screen.fill(WHITE)
-        sprite(sprite_img, 0, 0)
-        sprite(sprite_img2, 60, 0)
-        sprite(sprite_img3, 90, 0)
+        game_screen.fill(BLACK)
+        row = 0
+        screen_x = 0
+        screen_y = 0
+        offset = 0
+
+        for item in sprite_list:
+            if row == 8:
+                row = 0
+                screen_x = offset
+                screen_y += sprite_height
+
+
+            screen_position = (screen_x, screen_y)
+            sprite(item, screen_position)
+            screen_x += sprite_width
+            row += 1
+            if screen_y > HEIGHT:
+                screen_y = 0
+                offset += 160
         pygame.display.flip()
         clock.tick(30)
     pygame.quit()
---
+++ code/game/utilities/utility.py
@@ -0,0 +1,181 @@
+'''
+    A test for a pygame
+'''
+import pygame
+from pygame.locals import (QUIT, KEYDOWN, K_ESCAPE)
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+WIDTH = 1200
+HEIGHT = 900
+CAPTION = 'Utility Program'
+SCREEN_SIZE = (WIDTH, HEIGHT)
+FPS = 60
+
+ICON_FILE = 'y:/pyproject/resources/icon.png'
+
+
+pygame.init()
+game_screen = pygame.display.set_mode(SCREEN_SIZE)
+icon = pygame.image.load(ICON_FILE)
+pygame.display.set_caption(CAPTION)
+pygame.display.set_icon(icon)
+clock = pygame.time.Clock()
+
+SPRITE_X = 0
+SPRITE_Y = 0
+IMG_FILE = 'y:/Resources/images/stella_walk.png'
+img = []
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_000.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_001.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_002.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_003.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_004.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_005.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_006.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_007.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_008.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_009.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_010.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_011.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_012.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_014.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_015.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_016.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_017.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_018.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_019.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_020.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_021.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_022.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_023.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_024.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_025.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_026.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_027.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_028.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_029.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_030.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_031.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_032.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_033.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_034.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_035.png').convert())
+img.append(pygame.image.load('y:/pyproject/resources/test/sprites/e_036.png').convert())
+newimg = []
+for item in img:
+    newimg.append(pygame.transform.scale(item, (64, 64)))
+fly = []
+fly.append(pygame.image.load('y:/pyproject/resources/test/sprites/frame-1.png').convert())
+fly.append(pygame.image.load('y:/pyproject/resources/test/sprites/frame-2.png').convert())
+fly.append(pygame.image.load('y:/pyproject/resources/test/sprites/frame-3.png').convert())
+fly.append(pygame.image.load('y:/pyproject/resources/test/sprites/frame-4.png').convert())
+fly.append(pygame.image.load('y:/pyproject/resources/test/sprites/frame-5.png').convert())
+fly.append(pygame.image.load('y:/pyproject/resources/test/sprites/frame-6.png').convert())
+fly.append(pygame.image.load('y:/pyproject/resources/test/sprites/frame-7.png').convert())
+fly.append(pygame.image.load('y:/pyproject/resources/test/sprites/frame-8.png').convert())
+newfly = []
+for flyitem in fly:
+    newfly.append(pygame.transform.scale(flyitem, (64, 64)))
+
+imgload = pygame.image.load(IMG_FILE).convert()
+
+def sprite(sprite_surface, sprite_position):
+    '''
+        sprite function
+    '''
+    game_screen.blit(sprite_surface, sprite_position)
+
+
+def sprite_load(source_surface,
+                sprite_position,
+                sprite_size):
+    '''
+        new sprite image
+    '''
+    new_sprite = pygame.Surface(sprite_size)
+    new_sprite.set_colorkey(BLACK)
+    new_sprite.blit(source_surface,
+                    (0, 0),
+                    sprite_position)
+    return new_sprite
+
+
+def check_status():
+    '''
+        this checks the status of the game and return True is it should keep
+        running False is not.
+        There are no parameters to be passed and it returns a boolean as a
+        response.
+    '''
+    result = True
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                result = False
+        elif event.type == QUIT:
+            result = False
+    return result
+
+
+def main():
+    '''
+        main routine
+    '''
+    imgload.set_colorkey(BLACK)
+
+    start_posx = 0
+    start_posy = 0
+    sprite_width = 64
+    sprite_height = 64
+    sprite_size = (sprite_width, sprite_height)
+
+    sprite_list = []
+    cur_posx = start_posx
+    cur_posy = start_posy
+
+    for _ in range(8):
+        for _ in range(4):
+            sprite_pos = (cur_posx,
+                          cur_posy,
+                          sprite_width + cur_posx,
+                          sprite_height + cur_posy )
+            sprite_list.append(sprite_load(imgload, sprite_pos, sprite_size))
+            cur_posx += sprite_width
+        cur_posy += sprite_height
+        cur_posx = 0
+    cur_posx = start_posx
+    cur_posy = start_posy
+
+
+    RUNNING = True
+    screen_x = 300
+    screen_y = 300
+    screen_position = (screen_x, screen_y)
+    count = 0
+    counter = 0
+    looper = 0
+
+    while RUNNING:
+        RUNNING = check_status()
+        game_screen.fill(BLACK)
+        sprite(sprite_list[count], screen_position)
+        sprite(newimg[counter], (100, 300))
+        sprite(newfly[looper],(200, 300))
+        looper += 1
+        if looper >= len(fly):
+            looper = 0
+        count += 1
+        if count >= len(sprite_list):
+            count = 0
+        counter += 1
+        if counter >= len(img):
+            counter = 0
+        pygame.display.flip()
+        clock.tick(FPS)
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
