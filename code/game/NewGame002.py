299dd932df4100817beb75c6cc16bb3facb9b9d4
Denis Jackman <denis_jackman@hotmail.com>
2022-07-31 08:33:52 +0100

    daily commit

--- code/game/NewGame002.py
+++ code/game/NewGame002.py
@@ -1,12 +1,18 @@
+'''
+    another new game implementation
+'''
 import os
 import pygame
 def MainGame():
+    '''
+        main game routine
+    '''
     screen = pygame.display.set_mode((640,480))     # Set screen size of pygame window
     background = pygame.Surface(screen.get_size())  # Create empty pygame surface
     background.fill((255,255,255))                  # Fill the background white color (red,green,blue)
     background = background.convert()               # Convert Surface to make blitting faster
     screen.blit(background, (0, 0))
-    mainloop = True                    
+    mainloop = True
     while mainloop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
