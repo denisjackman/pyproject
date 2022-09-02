b90faa14f7f73d4c9c329452aa180413ee029a63
Denis <denis_jackman@hotmail.com>
2022-09-02 11:13:39 +0100

    Daily Commit 44

--- scratch/scratch.py
+++ scratch/scratch.py
@@ -56,85 +56,55 @@ def walk_through(wt_command_args):
     This walks through a directory and returns the name and path of each file.
     It takes one param which is the starting point directory for the search.
     It returns nothing.
-    It utilises check_file function to further check the file.
     """
 
     start_dir = wt_command_args["startdirectory"]
     verb = wt_command_args["verbosemode"]
-
-    count = 0
-    found = 0
-    result = []
-    for root, dirs, files in os.walk(start_dir, topdown=False):
-        for name in files:
-            if verb:
-                print(f"file found {name}")
-            if check_file_for_name(root, name, wt_command_args):
-                result.append(os.path.join(root, name))
-                found += 1
-                if verb:
-                    print(f"File found {count} {name}")
-
-            count += 1
-        for name in dirs:
-            if verb:
-                print(f"Directory found {count} {name}")
-            if check_file_for_name(root, name, wt_command_args):
-                result.append(os.path.join(root, name))
-                found += 1
-                if verb:
-                    print(f"Directory found {count} {name}")
-            count += 1
+    delete = wt_command_args["deletemode"]
+    listoffiles = os.listdir(start_dir)
+    result = list()
+    for entry in listoffiles:
+        fullpath = os.path.join(start_dir, entry)
+        if os.path.isdir(fullpath):
+            result = result + walk_through({"verbosemode": verb,
+            "deletemode": delete,
+            "startdirectory": fullpath})
+        else:
+            result.append(fullpath)
     return result
 
 
-def check_file_for_name(cff_root, cff_name, cff_command_args):
-    '''
-    check_file_for_name
 
-        :param : cff_root - the root directory for the file
-                 cff_name - the name of the file in question
-                 cff_command_args - a list of the command arguments
-
-        :return: boolean - True if the file exists
-                           False if it does not
-
-        check for the existence of a file in a list.
-        If it does return True.
-        If not return False.
-
-    '''
-    result = False
-    if cff_command_args["verbosemode"]:
-        print(f"Checking {cff_name} in {cff_root}")
-    # check the name tio see if has "(1)" in it
-    # if it does then record the size and delete the file
-    # if not then do nothing
-    # 
-    #delete_list = [".DS_Store",
-    #               ".AppleDouble",
-    #               "Thumbs.db",
-    #               ".pydevproject"]
-    #startswith = ["."]
-    #endswith = [".lnk"]
-    #for name in delete_list:
-    #    if cff_name == name:
-    #        if cff_command_args["deletemode"]:
-    #            if cff_command_args["verbosemode"]:
-    #                print(f'[FOUND] {os.path.join(cff_root, cff_name)} is to be deleted')
-    #            result = True
-
-
-    return result
 
 
 def main():
     """ This is the main routine for the program """
     print("Starting the sequence:")
+    search_list = [".DS_Store",
+                   ".AppleDouble",
+                   "Thumbs.db",
+                   ".pydevproject"]
+
     mainargs = getargs()
-    print(mainargs)
     filelist = walk_through(mainargs)
-    print(filelist)   
+    delete_list = list()
+
+    for item in filelist:
+        if item in search_list:
+            delete_list.append(item)
+        if item.find("(1)") != -1:
+            delete_list.append(item)
+        if item.endswith(".lnk"):
+            delete_list.append(item)
+
+    print(mainargs)
+    print(f"total count is {len(filelist)} - with {len(delete_list)} to be deleted")
+
+    if mainargs["deletemode"]:
+        for item in delete_list:
+            os.remove(item)
+            print(f"deleted {item}")
+
     print("finishing up and closing down:")
 
 
