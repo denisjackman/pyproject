419652d496887b6fdff89e188680efb61b5197c0
Denis <denis_jackman@hotmail.com>
2022-09-08 11:00:44 +0100

    fixed some linting issues

--- scratch/datascrape.py
+++ scratch/datascrape.py
@@ -13,41 +13,8 @@ __date__ = "$Date: 2022/09/05 09:00:00 $"
 __copyright__ = "Copyright (c) 2022 Denis J Jackman"
 __license__ = "Python"
 
-import os
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-
-def walk_through(wt_command_args):
-    """
-    walk_through function:
-
-        :param : wt_command_args - a list of the command arguments
-
-        :return: a list of files that have been found
-
-    This walks through a directory and returns the name and path of each file.
-    It takes one param which is the starting point directory for the search.
-    It returns nothing.
-    """
-
-    start_dir = wt_command_args["startdirectory"]
-    verb = wt_command_args["verbosemode"]
-    delete = wt_command_args["deletemode"]
-    listoffiles = list()
-    try:
-        listoffiles = os.listdir(start_dir)
-    except OSError as err:
-        print(f"OS error: {err} skipping {start_dir}")
-    result = list()
-    for entry in listoffiles:
-        fullpath = os.path.join(start_dir, entry)
-        if os.path.isdir(fullpath):
-            result = result + walk_through({"verbosemode": verb,
-            "deletemode": delete,
-            "startdirectory": fullpath})
-        else:
-            result.append(fullpath)
-    return result
 
 def datascrapemain():
     '''
@@ -56,7 +23,7 @@ def datascrapemain():
     print("Starting a data scrape")
 
     # set up the variables
-    listfiles = list()
+    listfiles = []
     checkfilepath = "Y:/Data/comment_votes.csv"
     #checkfilepath = "/home/share/disk1/workspace/Data/comment_votes.csv"
 
@@ -68,7 +35,6 @@ def datascrapemain():
     # step through the list of files
     for item in listfiles:
         if item != 'permalink':
-            #print(f"file: {item}")
             page = urlopen(item)
             soup = BeautifulSoup(page, features="lxml")
             stuff = soup.find("meta", property="og:title")
