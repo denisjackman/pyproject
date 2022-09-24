01d71714fa3d000ed056dc650cc0b387c20d2efc
Denis <denis_jackman@hotmail.com>
2022-09-24 06:51:09 +0100

    fixing the daily commit

--- scratch/pdfdoc.py
+++ scratch/pdfdoc.py
@@ -1,32 +1,20 @@
 '''
     pdf to doc converter
 '''
-from pdf2docx import parse
-from typing import tuple
+from pdf2docx import parse, Converter
 
 
-def convert_pdf_to_docx(input_file: str, output_file: str, pages: tuple = None) -> None:
+def convert_pdf_to_docx(pdf_file: str, doc_file: str):
     '''
         convert pdf to docx
     '''
-
-    if pages:
-        pages = [int(i) for i in list(pages) if i.isnumeric()]
-    result = parse(pdf_file=input_file,
-                   docx_with_path=output_file, pages=pages)
-    summary = {
-        "File": input_file, "Pages": str(pages), "Output File": output_file
-    }
-    # Printing Summary
-    print("## Summary ########################################################")
-    print("\n".join("{}:{}".format(i, j) for i, j in summary.items()))
-    print("###################################################################")
-    return result    
+    cv = Converter(pdf_file)
+    cv.convert(doc_file, start=0, end=None)
+    cv.close()
 
 def main():
     ''' main '''
-    convert_pdf_to_docx(input_file="test.pdf", output_file="test.docx")
-
+    convert_pdf_to_docx("test.pdf", "newtest.docx")
+ 
 if __name__ == '__main__':
     main()
-
