f82371190ebc985d4a98909c1f203711582e4a11
Denis <denis_jackman@hotmail.com>
2022-10-02 09:11:58 +0100

    Daily commit

--- scratch/djfileexplorer.py
+++ scratch/djfileexplorer.py
@@ -8,13 +8,18 @@ def main():
     '''
         main routine
     '''
-    rootdir = sys.argv[1]
-    if not os.path.isdir(rootdir):
-        print("Directory path {} does not exist. Exiting...".format(rootdir))
-        sys.exit()
-    for subdir, dirs, files in os.walk(rootdir):
-        for file in files:
-            print(os.path.join(subdir, file))
-
+    filelist = []
+    if len(sys.argv) == 2:
+        rootdir = sys.argv[1]
+        if not os.path.isdir(rootdir):
+            print(f"Directory path {rootdir} does not exist. Exiting...")
+            sys.exit()
+        for subdir, dirs, files in os.walk(rootdir):
+            for file in files:
+                filelist.append(os.path.join(subdir, file))
+        for file in filelist:
+            print(file)
+    else:
+        print("Usage: python3 djfileexplorer.py <directory path>")
 if __name__ == '__main__':
     main()
