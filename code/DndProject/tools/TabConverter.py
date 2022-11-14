0f121237f028a4da363297c3f89d3d46af17c8bf
Denis <denis_jackman@hotmail.com>
2022-11-14 05:29:21 +0000

    Daily Commit

--- code/DndProject/tools/TabConverter.py
+++ code/DndProject/tools/TabConverter.py
@@ -21,16 +21,18 @@ def main():
     '''
         main function
     '''
-    vc = ["aal","al","an","ang","atl","ax","axl","az","il","ill","is","iss","it","ith","itz","iz","otl","ov","oxl","us","uss","uz"]
-    vv = ["a","aka","aza","i","ila","illa","iza","o","ova","ovi"]
-    cv = ["cha","chka","cho","chta","hexo","hu","ka","kaa","kha","khi","ki","kii","kra","kry","ky","kzo","la","pa","qua","que","ra","rasa"]
-    cc = ["ch","cotl","cus","haat","has","hass","haz","hex","his","hiss","huax","kaar","kar","khat","kil","kis","kys","lan","mas","mass","mis","miss","pec","poc","ptar","pter","ras","rep","rept","sal","sith","tak","tan","tax","tec","tek","tep","than","this","tis","tl","tlac","tlan","tlax","trax","tzun","val","var","vas","ven","vil","vis","xh","xlan","yxl","zaal","zah","zak","zal","zla"]
+    adj = ["abhorrent","abominable","ancient","black","blasphemous","crawling","creeping","crypt","damned","dark","dead","deep","dimensional","dread","elder","eldritch","esoteric","evil","faceless","flying","formless","gaunt","hideous","horrific","living","loathsome","lurking","mad","mist","night","red","shadow","shambling","shunned","sightless","slithering","star","tomb","white","winged"]
+    noun = ["beasts","burrowers","children","crawlers","creatures","creepers","dead","devourers","dwellers","followers","hounds","horrors","hunters","ones","ooze","outsiders","polyps","prowlers","servants","shamblers","spawn","stalkers","things","walkers","worms","young"]
+    prefv = ["aza","bya","ctha","gha","ghata","golo","itha","kha","kla","nya","nyag","nyarla","tsa"]
+    prefc = ["ab","ash","ashtor","azar","azaz","byak","byal","chaug","cthon","cthug","cthul","dag","faug","ghatan","hal","hast","ith","ithaq","kyn","len","leng","nyar","nyog","r'y","shog","shub","soth","tsag","tsath","y'g","yith","yog"]
+    suff = ["ash","aten","azar","azash","ekesh","eth","gash","gha","gon","goth","gua","hotep","hoth","hu","ikesh","kesh","khee","lash","loth","lyeh","nac","nar","nash","oggua","on","onac","oth","othoa","rath","tha","thal","thoa","thotep","thoth","ugha","ulhu","urath","ze"]
 
-    with open(f"{FILEPATH}/../referencedata/LizardNames.json", "w", encoding='utf8') as file:
-        json.dump({"lizard_vc": vc,
-                   "lizard_vv": vv,
-                   "lizard_cv": cv,
-                   "lizard_cc": cc},
+    with open(f"{FILEPATH}/../referencedata/LovecraftCreatures.json", "w", encoding='utf8') as file:
+        json.dump({"lovecraft_adjective": adj,
+                   "lovecraft_noun": noun,
+                   "lovecraft_prefixv": prefv,
+                   "lovecraft_prefixc": prefc,
+                   "lovecraft_suffix": suff},
                   file,
                   indent=4,
                   ensure_ascii=False)
