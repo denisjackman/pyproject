66771d81046a5b22942ee11578f55ee6669f40f7
Denis <denis_jackman@hotmail.com>
2022-12-05 10:34:25 +0000

    Daily Commit

---
+++ scratch/notworking/example.py
@@ -0,0 +1,19 @@
+'''
+    example
+'''
+grp_content = []
+print(grp_content)
+with open('Address_Groups.txt', encoding='utf8') as f:
+    for line in f:
+        if line.split(' ')[1] == "shared":
+            grp_name = line.split(' ')[3]
+        if '[' in line:
+            content = line.split('[')[1]
+            grp_content.append(content)
+        else:
+            content = line.split(' ')[5]
+            grp_content.append(content)
+print('final list : ', grp_content)
+print ('list len  : ', len(grp_content))
+for item in grp_content:
+    print('Item    : ',item.strip())
