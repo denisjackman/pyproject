e6355d02a64c24002ed86923db3fec1faa2e134f
Denis <denis_jackman@hotmail.com>
2022-12-18 11:06:41 +0000

    Daily Commit

---
+++ scratch/toolkit.py
@@ -0,0 +1,21 @@
+''' toolkit.py '''
+
+from pathlib import Path
+import json
+
+FILEPATH = Path(__file__).parent
+
+def main():
+    ''' main function '''
+    filename = f"{FILEPATH}/allMimeTypes.json"
+    with open(filename, "r", encoding='utf8') as file:
+        result = json.load(file)
+    print(result)
+    mime_types = []
+    mime_file = []
+    for item in result: 
+        item
+        
+
+if __name__ == "__main__":
+    main()
