07375e9f013dbf1977085e3b66861125c5706c17
Denis <denis_jackman@hotmail.com>
2022-12-20 10:48:19 +0000

    daily commit

--- scratch/toolkit.py
+++ scratch/toolkit.py
@@ -10,10 +10,21 @@ def main():
     filename = f"{FILEPATH}/allMimeTypes.json"
     with open(filename, "r", encoding='utf8') as file:
         result = json.load(file)
-    print(result)
+
     mime_types = []
     mime_file = []
+    test_example = "text/x-python"
 
+    for item in result:
+        print(item.values())
+        print(item.keys())
+        mime_types.append(item.keys())
+        mime_file.append(item.values())
+
+
+    print(mime_types)
+    print(mime_file)
+    print(test_example)
 
 if __name__ == "__main__":
     main()
