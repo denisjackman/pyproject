d3c37f1aa81e1fef0cca59dfd898126b716c0545
Denis <denis_jackman@hotmail.com>
2023-03-16 08:33:43 +0000

    Daily Commit

---
+++ scratch/armstrong.py
@@ -0,0 +1,24 @@
+''' this is a function to check if a number is an armstrong number or not'''
+
+def armstrong(number):
+    ''' this is a function to check if a number is an armstrong number or not'''
+    result = False
+    total = 0
+    temp = number
+
+    while temp > 0:
+        digit = temp % 10
+        total += digit ** 3
+        temp //= 10
+    if number == total:
+        result = True
+    return result
+
+def main():
+    ''' this is a function to check if a number is an armstrong number or not'''
+    print (f"662 is an armstrong number {armstrong(662)}")
+    print (f"663 is an armstrong number {armstrong(663)}")
+    print (f"407 is an armstrong number {armstrong(407)}")
+    
+if __name__ == "__main__":
+    main()
