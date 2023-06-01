033e38e87f74a004295629d8bb36b875488c443f
Denis <denis_jackman@hotmail.com>
2023-06-01 13:52:00 +0100

    Daily Commit

--- scratch/djdaily.py
+++ scratch/djdaily.py
@@ -1,7 +1,7 @@
 ''' utility to find zip files '''
 import os
 
-DIRECTORY = "Y:\\Resources"
+DIRECTORY = "X:\\Pictures"
 
 def walk_through(wt_command_args):
     """
@@ -19,29 +19,38 @@ def walk_through(wt_command_args):
     start_dir = wt_command_args["startdirectory"]
     verbosemode = wt_command_args["verbosemode"]
     if verbosemode:
-        print(f"[+] Starting walk through {start_dir}")
+        print(f"[o] Starting walk through {start_dir}")
     count = 0
     result = []
     if verbosemode:
-        print(f"[+] Searching for files in {start_dir}")
+        print(f"[o] Searching for files in {start_dir}")
     for root, _, files in os.walk(start_dir, topdown=False):
         for name in files:
             result.append(os.path.join(root, name))
             count += 1
     if verbosemode:
-        print(f"[+] Finished walk through {start_dir}")
-        print(f"[+] Found {count} files")
+        print(f"[o] Finished walk through {start_dir}")
+        print(f"[o] Found {count} files")
     return result
 
 def main():
     ''' main function '''
     print("[+] Starting")
-    commands = {"verbosemode":True, "deletemode":False, "startdirectory":DIRECTORY}
+    commands = {"verbosemode":False, "deletemode":False, "startdirectory":DIRECTORY}
     result = walk_through(commands)
     pdffound = 0
     txtfound = 0
+    jpgfound = 0
+    pngfound = 0
+    movfound = 0
+    aaefound = 0
+
     pdflist = []
     txtlist = []
+    jpglist = []
+    pnglist = []
+    movlist = []
+    aaelist = [] 
 
     for item in result:
         if item.endswith(".txt"):
@@ -52,8 +61,35 @@ def main():
             pdflist.append(item)
             pdffound += 1
 
-    print(f"[+] PDF Found {pdffound} files")
-    print(f"[+] TXT Found {txtfound} files")
+        if item.endswith(".jpg"):
+            jpglist.append(item)
+            jpgfound += 1
+
+        if item.endswith(".png"):
+            pnglist.append(item)
+            pngfound += 1
+
+        if item.endswith(".jpeg"):
+            jpglist.append(item)
+            jpgfound += 1
+
+        if item.endswith(".mov"):
+            movlist.append(item)
+            movfound += 1
+
+        if item.endswith(".aae"):
+            aaelist.append(item)
+            aaefound += 1
+
+    print("[-] File Inventory ")
+    print("-------------------------------")
+    print(f"[*] JPG Found {jpgfound} files")
+    print(f"[*] PDF Found {pdffound} files")
+    print(f"[*] TXT Found {txtfound} files")
+    print(f"[*] PNG Found {pngfound} files")
+    print(f"[*] MOV Found {movfound} files")
+    print(f"[*] AAE Found {aaefound} files")
+    print("-------------------------------")
     print("[+] Finished")
 
 if __name__ == '__main__':
