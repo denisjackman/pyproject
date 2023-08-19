f340ae4b7c98b503b7f019353afc7c051b80aa38
Denis <denis_jackman@hotmail.com>
2023-08-19 09:20:37 +0100

    Daily commit

--- code/viscous/djzip.py
+++ code/viscous/djzip.py
@@ -1,39 +1,68 @@
 ''' zip file cracker '''
 import zipfile
-import optparse
-import sys
 from threading import Thread
+import os
+import sys
+import getopt
+
+
+def getargs():
+    '''
+        get the argurments from the command line
+    '''
+    st_commands = f'{os.path.basename(__file__)} -v <True/False> -f <Zipfile> -d <Dictionary> -h <help>'
+    argv = sys.argv[1:]
+    commands = "hvf:d:"
+    long_commands = ["help", "verbose", "file=", "dictionary="]
+    verbosemode = False
+    dictionary = ''
+    zipfilename = ''
+    try:
+        clopts, args = getopt.getopt(argv, commands, long_commands)
+    except getopt.GetoptError:
+        print(st_commands)
+        sys.exit(2)
+    for clopt, arg in clopts:
+        if clopt in ('-h', "--help"):
+            print(st_commands)
+            sys.exit()
+        elif clopt in ("-v", "--verbose"):
+            verbosemode = True
+        elif clopt in ("-d", "--dictionary"):
+            dictionary = arg
+        elif clopt in ("-f", "--file"):
+            zipfilename = arg
+    return {"verbosemode": verbosemode,
+            "dictionary": dictionary,
+            "zipfile": zipfilename}
 
 def extractFile(zFile, password):
     ''' extract file from zip file '''''
-    passcheck = False
+    result = False
     try:
-        zFile.extractall(pwd=password)
-        passcheck = True
-    except:  # pylint: disable=bare-except
-        pass
-    if passcheck:
-        print(f'[+] Found password {password}\n')
-
+        zFile.extractall(pwd=password.encode())
+    except Exception as err:
+        result = False
+    else:
+        result = True
+    return result
 
 def main():
     ''' main function '''
-    parser = optparse.OptionParser('usage%prog -f <zipfile> -d <dictionary>')
-    parser.add_option('-f', dest='zname', type='string', help='specify zip file')
-    parser.add_option('-d', dest='dname', type='string', help='specify dictionary file')
-    (options, args) = parser.parse_args()
-    if (options.zname is None) | (options.dname is None):
-        print(f"{parser.usage}")
-        sys.exit(0)
-    else:
-        zname = options.zname
-        dname = options.dname
-    with zipfile.ZipFile(zname) as zFile:
-        with open(dname, 'r', encoding='utf-8-sig') as passFile:
+    main_args = getargs()
+    do_threading = False
+    with zipfile.ZipFile(main_args["zipfile"]) as zFile:
+        with open(main_args["dictionary"], 'r', encoding='utf-8-sig') as passFile:
             for line in passFile.readlines():
                 password = line.strip('\n')
-                t = Thread(target=extractFile, args=(zFile, password))
-                t.start()
+                if do_threading:
+                    t = Thread(target=extractFile, args=(zFile, password))
+                    t.start()
+                else:                    
+                    answer = extractFile(zFile, password)
+                    if answer:
+                        print(f'[+] Password = {password}')
+                        break
 
 if __name__ == '__main__':
     main()
