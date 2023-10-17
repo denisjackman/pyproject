666e957f830568f99de44fb3314a49f6daed5f6b
Denis <denis_jackman@hotmail.com>
2023-10-17 08:57:11 +0100

    Daily commit

--- code/tooling/pangolin.py
+++ code/tooling/pangolin.py
@@ -8,10 +8,11 @@ This is a os utility tool
 '''
 import os
 import sys
-from tqdm import tqdm
+import hashlib
+#from tqdm import tqdm
 # pylint: disable=C0413
 sys.path.append(os.path.realpath('../..'))
-from jackmanimation.utilities.fileutility import walk_through
+#from jackmanimation.utilities.fileutility import walk_through
 from jackmanimation.utilities.fileutility import getargs
 
 __author__ = "Denis J Jackman (denis_jackman@hotmail.com)"
@@ -21,6 +22,35 @@ __copyright__ = "Copyright (c) 2022 Denis J Jackman"
 __license__ = "Python"
 
 PANGOLIN_TARGET_DIR = 't:'
+
+def find_duplicate_files(directory_path):
+    """Steps through a directory of files and compares each file to find duplicates and delete the duplicates.
+
+    Args:
+    directory_path (str): The path to the directory to scan for duplicates.
+    """
+
+    # Create a dictionary to store the hashes of the files.
+    file_hashes = {}
+    count = 0 
+    file_count = 0
+    # Iterate through all the files in the directory.
+    for root, directories, files in os.walk(directory_path):
+        for item_file in files:
+            file_count += 1
+            # Calculate the hash of the file.
+            file_path = os.path.join(root, item_file)
+            file_hash = hashlib.md5(open(file_path, "rb").read()).hexdigest()
+
+            # If the hash is already in the dictionary, the file is a duplicate.
+            if file_hash in file_hashes:
+                print(f'[0] Duplicate found: {file_path} {file_hashes[file_hash]}')
+                count += 1
+                os.remove(file_path)
+            else:
+                file_hashes[file_hash] = file_path
+    print(f'[-] {count} duplicates found')
+    print(f'[-] {file_count} files found')
 
 def validate_file_contents(vfc_file1, vfc_file2):
     ''' validate_file_contents '''
@@ -41,25 +71,30 @@ def main():
     pango_mainargs = getargs()
     print("[+] pangolin getting args")
     print(f'[-] {pango_mainargs}')
-    print("[+] pangolin walking through files")
-    pango_filelist = walk_through(pango_mainargs)
-    compare_list = pango_filelist
-    print(f'[-] {len(pango_filelist)} files found')
-    print("[+] pangolin validating files")
-
-    duplicate_count = 0
-    list_store = compare_list
-    for item in tqdm(pango_filelist, total=len(pango_filelist), unit=' item'):
-        for pango_file in tqdm(compare_list, total=len(compare_list), unit=' item'):
-            if pango_file != item:
-                if validate_file_contents(pango_file, item):
-                    #print(f'[-] {pango_file} is a duplicate of {item}')
-                    list_store.remove(pango_file)
-                    duplicate_count += 1
-        compare_list = list_store
-
-    print(f'[-] {duplicate_count} duplicates found')
-    print(f'[-] {len(compare_list)} unique files found')
+    find_duplicate_files(pango_mainargs["startdirectory"])
+#    print("[+] pangolin walking through files")
+#    pango_filelist = walk_through(pango_mainargs)
+#    compare_list = pango_filelist
+#    print(f'[-] {len(pango_filelist)} files found')
+#    print("[+] pangolin validating files")
+#    total_duplicates = 0
+#    loop_count = 0
+#    for item in tqdm(pango_filelist, total=len(pango_filelist), unit=' item'):
+#        duplicate_count = 0
+#        list_store = compare_list
+#        for pango_file in tqdm(compare_list, total=len(compare_list), unit=' item'):
+#            if pango_file != item:
+#                if validate_file_contents(pango_file, item):
+#                    list_store.remove(pango_file)
+#                    duplicate_count += 1
+#                    total_duplicates += 1
+#        if duplicate_count == 0:
+#            break
+#        compare_list = list_store
+#
+#    print(f'[-] {loop_count} iterations')
+#    print(f'[-] {total_duplicates} duplicates found')
+#    print(f'[-] {len(compare_list)} unique files found')
     print('[=] pangolin shutting down')
 
 if __name__ == '__main__':
