d991cd62fd48d88bc7d050316c95f2d64bd49d4b
Denis <denis_jackman@hotmail.com>
2023-11-04 10:28:16 +0000

    daily commit

--- code/questions/questions.py
+++ code/questions/questions.py
@@ -24,13 +24,20 @@ def question3(a, b, c):
     else:
         return c
     return 0
-    
+
+def question4():
+    ''' This is a question 4'''
+    x = [1, 2, 3, 4]
+    y = [sum(x[0:i-1]) for i in range(0,4)]
+    print(f'[-] question 4 : {y}')
+
 def main():
     ''' This is the main function'''
     print('[+] This is the main function starting')
     question1()
     #question2()
     print(f'[-] question 3 {question3(10,5,2)}')
+    question4()
     print('[+] This is the main function ending')
 
 if __name__ == '__main__':
