61eee39a2a308a7f4f61459dbee2e194c348fc38
Denis <denis_jackman@hotmail.com>
2023-12-06 17:47:00 +0000

    daily commit

---
+++ code/questions/nuquestion.py
@@ -0,0 +1,15 @@
+'''Questions for the nu question type.'''
+
+def main():
+    '''main function'''
+    print('[-] main function starting')
+    a = 10
+    b = 10
+    print(f'[-] a is b {a is b}')
+    print(f'[-] a == b {a == b}')
+    print('[-] main function ending')
+
+if __name__ == '__main__':
+    print('[+] Running: code/questions/nuquestion.py ')
+    main()
+    print('[+] Done: code run complete')
