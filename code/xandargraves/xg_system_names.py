f544f6103addc0ffe6a13a0b4b0e693eed301b64
Denis <denis_jackman@hotmail.com>
2024-01-01 10:11:19 +0000

    Daily Commit

---
+++ code/xandargraves/xg_system_names.py
@@ -0,0 +1,14 @@
+''' This is a list of systems for the imperial side of the Xander Graves UNiverse'''
+
+DATA_DIR = 'G:/Writing/Fiction/Stories/Xander graves/Data'
+
+def main():
+    ''' This is the main function'''
+    print('[-] This is the main function starting')
+    print(f'[-] {DATA_DIR}')
+    print('[-] This is the main function ending')
+
+if __name__ == '__main__':
+    print('[+] This is the xg_system_names program starting')
+    main()
+    print('[+] This is the xg_system_names program ending')
