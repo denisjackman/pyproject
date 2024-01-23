8582d5c5c482b48d7f26de217fd160b6090458df
Denis <denis_jackman@hotmail.com>
2024-01-23 09:01:26 +0000

    Daily Commit

--- code/project/project.py
+++ code/project/project.py
@@ -1,6 +1,7 @@
 '''Project python script'''
 import os
 import sys
+import pandas as pd
 
 # pylint: disable=C0413
 sys.path.append(os.path.realpath('../..'))
@@ -12,7 +13,9 @@ __date__ = "$Date: 2024/01/22 12:48:00 $"
 __copyright__ = "Copyright (c) 2024 Denis J Jackman"
 __license__ = "Python"
 
-TARGET_DIR = 'Z:/Store'
+TARGET_DIR = 'G:'
+TARGET_FILE = 'Z:/Store/target/filelist.xlsx'
+PROJECT_FILE = 'Z:/Store/target/projectlist.xlsx'
 
 def main():
     '''Main function'''
@@ -23,8 +26,14 @@ def main():
                         "targetdirectory": TARGET_DIR}
     project_filelist = walk_through(project_mainargs)
     print(f'[-] {len(project_filelist)} files found')
+    df = pd.DataFrame(project_filelist)
+    writer = pd.ExcelWriter(PROJECT_FILE, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='all files')
     final_filelist = sift_files(project_filelist)
     print(f'[-] {len(final_filelist)} files found')
+    df = pd.DataFrame(final_filelist)
+    df.to_excel(writer, sheet_name='xl files')
+    writer.close()
     print('[-] Main Function Finished.')
 
 def sift_files(sift_list):
