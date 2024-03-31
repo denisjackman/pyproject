6a8ba817bd778a03b4b5fb7bceaea994fce4ab8d
Denis <denis_jackman@hotmail.com>
2024-03-31 10:20:28 +0100

    daily commit

--- code/tkfolder/tkplay.py
+++ code/tkfolder/tkplay.py
@@ -5,17 +5,33 @@ import tkinter as tk
 
 def build_window(bw_name, bw_geometry):
     ''' build a window '''
+    print('[*] build_window: start')
+    print('[-] build_window: name: %s, geometry: %s' % (bw_name, bw_geometry))
     bw_window = tk.Tk()
     bw_window.title(bw_name)
     bw_window.geometry(bw_geometry)
+    print('[*] build_window: end')
     return bw_window
+
 
 def main():
     ''' main function '''
     print('[*] tk example start')
     main_window = build_window('tk example', '400x400')
-    label = tk.Label(main_window, text='tk example label in my window')
+    label = tk.Label(main_window,
+                     text='tk example label in my window',
+                     bg='red')
     label.pack()
+    label1 = tk.Label(main_window,
+                      text='tk example label1 in my window',
+                      font=('Arial', 15, 'bold'),
+                      bg='yellow')
+    label1.pack()
+    label2 = tk.Label(main_window,
+                      text='tk example label2 in my window',
+                      font=('Monospace', 16, 'italic'),
+                      bg='green')
+    label2.pack()
     main_window.mainloop()
     print('[*] tk example end')
 
