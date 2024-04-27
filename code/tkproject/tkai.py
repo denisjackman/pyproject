f8e4d08adccf878dd32b0163c7488b0e191fc11c
Denis <denis_jackman@hotmail.com>
2024-04-27 13:50:49 +0100

    Daily Commit

--- code/tkproject/tkai.py
+++ code/tkproject/tkai.py
@@ -10,20 +10,20 @@ from jackmanimation.gameitems.gamefunctions import credscheck  # noqa: E402
 
 def generate_response(gr_prompt):
     ''' Use the ChatGPT model to generate a response '''
-    model_engine = "gpt-3.5-turbo-instruct"
+    gr_model_engine = "gpt-3.5-turbo-instruct"
     gr_prompt = f"{gr_prompt}\n"
     gr_message = ''
     try:
-        completions = openai.Completion.create(engine=model_engine,
-                                               prompt=gr_prompt,
-                                               max_tokens=1024,
-                                               n=1,
-                                               stop=None,
-                                               temperature=0.5)
+        gr_completions = openai.Completion.create(engine=gr_model_engine,
+                                                  prompt=gr_prompt,
+                                                  max_tokens=1024,
+                                                  n=1,
+                                                  stop=None,
+                                                  temperature=0.5)
     except Exception as e:
         gr_message = f'Error in generating response : {e}'
     if gr_message == '':
-        gr_message = completions.choices[0].text
+        gr_message = gr_completions.choices[0].text
     return gr_message.strip()
 
 
