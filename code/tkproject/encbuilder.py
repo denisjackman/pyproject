8947f47d4769332ca8bb33580a03173e96a7a047
Denis <denis_jackman@hotmail.com>
2024-05-28 09:01:36 +0100

    daily commit

---
+++ code/tkproject/encbuilder.py
@@ -0,0 +1,15 @@
+''' encounter builder'''
+
+
+def main():
+    ''' encounter builder'''
+    print("[Encounter Builder] Starting...")
+    print("[Encounter Builder] Generating a monster...")
+    print("[Encounter Builder] Generating a trap...")
+    print("[Encounter Builder] Generating a treasure...")
+    print("[Encounter Builder] Generating a name...")
+    print("[Encounter Builder] Complete")
+
+
+if __name__ == "__main__":
+    main()
