aae62fd8f43bb90784a5e3d2307f773a3c1ce0d0
Denis <denis_jackman@hotmail.com>
2024-05-30 08:26:26 +0100

    daily commit

--- code/tkproject/encbuilder.py
+++ code/tkproject/encbuilder.py
@@ -4,6 +4,65 @@ import csv
 ENC_FILE = "Z:/Maturam/encbuilder.csv"
 
 
+def enc_buget_multiplier(ebm_num_creatures, ebm_party_size):
+    ''' encounter budget multiplier '''
+    ebm_result = 1
+    choice = ebm_num_creatures
+    party = ebm_party_size
+    match choice:
+        case 1:
+            match party:
+                case 2:
+                    return 0.67
+                case party if 3 <= party <= 5:
+                    return 1.00
+                case _:
+                    return 1.50
+        case 2:
+            match party:
+                case 2:
+                    return 0.5
+                case party if 3 <= party <= 5:
+                    return 0.67
+                case _:
+                    return 1.00
+        case choice if 3 <= choice <= 6:
+            match party:
+                case 2:
+                    return 0.4
+                case party if 3 <= party <= 5:
+                    return 0.5
+                case _:
+                    return 0.67
+        case choice if 7 <= choice <= 10:
+            match party:
+                case 2:
+                    return 0.33
+                case party if 3 <= party <= 5:
+                    return 0.4
+                case _:
+                    return 0.33
+        case choice if 11 <= choice <= 14:
+            match party:
+                case 2:
+                    return 0.25
+                case party if 3 <= party <= 5:
+                    return 0.33
+                case _:
+                    return 0.25
+        case choice if choice >= 15:
+            match party:
+                case 2:
+                    return 0.20
+                case party if 3 <= party <= 5:
+                    return 0.25
+                case _:
+                    return 0.20
+        case _:
+            print("[-] Error Defaulting to 1.0")
+            return ebm_result
+
+
 def enc_read_csv_file(ercf_filename):
     ''' read csv file '''
     ercf_result = []
@@ -30,10 +89,14 @@ def main():
         if not main_party_size.isnumeric():
             print("[+] Please enter a number")
         else:
-            print(f'[-] {enc_data[0]}')
-            print(f'[-] {enc_data[int(main_input)][0]}'
-                  f' {enc_data[int(main_input)][1]}'
-                  f' {enc_data[int(main_input)][int(main_party_size)-1]}')
+            enc_budget = enc_data[int(main_input)][int(main_party_size)-1]
+        main_monster = input("[-] Monster: ")
+        if not main_monster.isnumeric():
+            print("[+] Please enter a number")
+        else:
+            main_bm = enc_buget_multiplier(int(main_monster), int(main_party_size))
+            final_enc_budget = float(enc_budget) * main_bm
+            print(f'[*] {int(final_enc_budget)}') 
     # print("[Encounter Builder] Generating a monster...")
     # print("[Encounter Builder] Generating a trap...")
     # print("[Encounter Builder] Generating a treasure...")
