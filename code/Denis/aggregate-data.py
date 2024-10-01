6c10beb65f02386c3681cd39fb7a825af97cc688
Denis Jackman <denis_jackman@hotmail.com>
2024-10-01 08:57:56 +0100

    Daily Commit

---
+++ code/Denis/aggregate-data.py
@@ -0,0 +1,13 @@
+''' medisafe analysis'''
+
+DIR_LIST = ["20111103", "20161220", "20190331", "20240926"]
+BASE_DIR = "Y:/Data/health/"
+AGGREGATE_DIR = "aggregate"
+
+
+def main():
+    ''' main function'''
+
+
+if __name__ == '__main__':
+    main()
---
+++ code/Denis/notes.md
@@ -0,0 +1,6 @@
+# Notes for this project 
+
+## Links
+
+* [A code repo](https://github.com/markwk/qs_ledger/tree/master)
+* [A Blog Entry](http://www.markwk.com/data-analysis-for-apple-health.html)
